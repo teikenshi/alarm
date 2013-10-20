d63d36cdb59d71655989515f53347738a0bf47e2
zheng xianzi <heanzza@zheng-no-MacBook-Air.local>
2013-10-21 00:40:52 +0900

    first commit

---
+++ SettingVC.h
@@ -0,0 +1,13 @@
+//
+//  SettingVC.h
+//  AlamTest1
+//
+//  Created by zheng xianzi on 2013/10/12.
+//  Copyright (c) 2013年 zheng xianzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingVC : UIViewController
+
+@end
---
+++ SettingVC.m
@@ -0,0 +1,38 @@
+//
+//  SettingVC.m
+//  AlamTest1
+//
+//  Created by zheng xianzi on 2013/10/12.
+//  Copyright (c) 2013年 zheng xianzi. All rights reserved.
+//
+
+#import "SettingVC.h"
+
+@interface SettingVC ()
+
+@end
+
+@implementation SettingVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
